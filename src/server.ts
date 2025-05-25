51e27c797997d28794663c2c4b71cf543d8bf69d
aditya038 <adityachauhan0605@gmail.com>
2025-05-26 00:12:33 +0530

    Replace server.ts with Netlify compatible version

--- src/server.ts
+++ src/server.ts
@@ -7,10 +7,12 @@ import {
 import express from 'express';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { CommonEngine } from '@angular/ssr/node'
+import { render } from '@netlify/angular-runtime/common-engine.mjs'
 
 const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 const browserDistFolder = resolve(serverDistFolder, '../browser');
-
+const commonEngine = new CommonEngine()
 const app = express();
 const angularApp = new AngularNodeAppEngine();
 
@@ -49,6 +51,16 @@ app.use('/**', (req, res, next) => {
     .catch(next);
 });
 
+export async function netlifyCommonEngineHandler(request: Request, context: any): Promise<Response> {
+  // Example API endpoints can be defined here.
+  // Uncomment and define endpoints as necessary.
+  // const pathname = new URL(request.url).pathname;
+  // if (pathname === '/api/hello') {
+  //   return Response.json({ message: 'Hello from the API' });
+  // }
+
+  return await render(commonEngine)
+}
 /**
  * Start the server if this module is the main entry point.
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
